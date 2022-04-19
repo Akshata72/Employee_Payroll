e72801477e221e3737ac1c30fc6dfe085523f8db
Akshata72 <sableakshata880@gmail.com>
2022-04-19 08:19:14 +0530

    Added:UC2-Create_Employee_Payroll_Table

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -2,3 +2,11 @@ Create database payroll_service;
 go
 use payroll_service
 go
+create Table Employee_Payroll(
+ID INT identity(1,1) not null Primary key,
+First_Name varchar(50),
+Last_Name varchar(50),
+Salary bigint,
+StartDate Datetime
+);
+select * from Employee_Payroll;
