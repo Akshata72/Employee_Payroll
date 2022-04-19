45a9dab715e5230d17f4477f5ac5330a25d5480c
Akshata72 <sableakshata880@gmail.com>
2022-04-19 08:36:01 +0530

    Added:UC3-Insert_Data_into_Employee_Payroll_Table

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -10,3 +10,9 @@ Salary bigint,
 StartDate Datetime
 );
 select * from Employee_Payroll;
+insert into Employee_Payroll(First_Name,Last_Name,Salary,StartDate) values 
+            ('Akshata','Panchal',15000,'2022-04-19'),
+			('Aditya','Shinde',18000,'2022-04-19'),
+			('Vishal','Gangane',20000,'2022-04-19'),
+			('Vaishnavi','Sabale',17000,'2022-04-19'),
+			('Supriya','Kadam',22000,'2022-04-19');
